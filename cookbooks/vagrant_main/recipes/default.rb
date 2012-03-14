0a2e38b2d7af1692ae2bdcc6728507e266df053f
Sergey Storchay <r8@r8.com.ua>
2012-03-14 21:48:37 +0200

    Added apt recipe

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,8 +1,14 @@
+require_recipe "apt"
 require_recipe "git"
 require_recipe "zsh"
 require_recipe "oh-my-zsh"
 
 # Install packages
-%w{ mc subversion }.each do |a_package|
+%w{ screen mc subversion }.each do |a_package|
   package a_package
+end
+
+# Install ruby gems
+%w{ rake }.each do |a_gem|
+  gem_package a_gem
 end
