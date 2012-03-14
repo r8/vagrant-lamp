dc50d9ad05ba22d4a4f9abd764aae1e4b23a2657
Sergey Storchay <r8@r8.com.ua>
2012-03-14 22:40:26 +0200

    Added apache, mysql and php

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -2,6 +2,9 @@ require_recipe "apt"
 require_recipe "git"
 require_recipe "zsh"
 require_recipe "oh-my-zsh"
+require_recipe "apache2"
+require_recipe "mysql"
+require_recipe "php"
 
 # Install packages
 %w{ screen mc subversion }.each do |a_package|
