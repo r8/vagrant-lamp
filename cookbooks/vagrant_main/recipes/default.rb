59b902fd4c930cdfca9b189263e2a24ba243ca0f
Sergey Storchay <r8@r8.com.ua>
2012-03-15 08:07:48 +0200

    Added mod_vhost_alias

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -3,15 +3,22 @@ require_recipe "git"
 require_recipe "zsh"
 require_recipe "oh-my-zsh"
 require_recipe "apache2"
+require_recipe "apache2::mod_rewrite"
 require_recipe "mysql"
 require_recipe "php"
+require_recipe "apache2::mod_php5"
 
 # Install packages
-%w{ screen mc subversion }.each do |a_package|
+%w{ vim screen mc subversion }.each do |a_package|
   package a_package
 end
 
 # Install ruby gems
 %w{ rake }.each do |a_gem|
   gem_package a_gem
+end
+
+# Setup vhost_alias
+apache_module "vhost_alias" do
+  conf true
 end
