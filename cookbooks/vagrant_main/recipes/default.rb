8b604cbdad96a06889fe16c877f633869f7be377
Sergey Storchay <r8@r8.com.ua>
2012-03-18 17:19:50 +0200

    Added xdebug

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -50,3 +50,16 @@ bash "debconf_for_phpmyadmin" do
   code "debconf-set-selections /tmp/phpmyadmin.deb.conf"
 end
 package "phpmyadmin"
+
+# Install Xdebug
+php_pear "xdebug" do
+  action :install
+end
+template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
+  source "xdebug.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+  notifies :restart, resources("service[apache2]"), :delayed
+end
