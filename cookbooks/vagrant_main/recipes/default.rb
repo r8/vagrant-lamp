ac31e7a3e98bc21a5c69ad0bdd8f9aca8ef49fde
Esbjörn Eriksson <esbjorn@bazooka.se>
2012-09-15 01:09:12 +0200

    Add php mysql.
    Conflicts:
    	cookbooks/vagrant_main/recipes/default.rb

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -6,6 +6,7 @@ include_recipe "apache2::mod_rewrite"
 include_recipe "apache2::mod_ssl"
 include_recipe "mysql::server"
 include_recipe "php"
+include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
 include_recipe "composer"
 
