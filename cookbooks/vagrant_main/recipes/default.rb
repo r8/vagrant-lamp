9874a8e1e63d3a0c92963494252d16dd3fd21053
Sergey Storchay <r8@r8.com.ua>
2012-11-06 08:40:09 +0200

    Install Composer, closed #10

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -129,3 +129,11 @@ end
 %w{ libmysqlclient16 percona-toolkit }.each do |a_package|
   package a_package
 end
+
+# Install Composer
+bash "composer" do
+  code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+  EOH
+end
