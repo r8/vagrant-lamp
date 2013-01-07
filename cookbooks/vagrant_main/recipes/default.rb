5282234b89ed4aeef32cb50e5b4ae896869d30d4
Eelke Blok <eelke@blokspeed.net>
2013-01-07 11:53:52 +0100

    Install graphviz
    Hoping call graph generation would work from webgrind. Not quite there yet.
    
    Conflicts:
    	cookbooks/vagrant_main/recipes/default.rb

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -12,7 +12,7 @@ include_recipe "composer"
 include_recipe "drush"
 
 # Install packages
-%w{ debconf vim screen tmux mc subversion curl make g++ libsqlite3-dev libxml2-utils lynx links}.each do |a_package|
+%w{ debconf vim screen tmux mc subversion curl make g++ libsqlite3-dev graphviz libxml2-utils lynx links}.each do |a_package|
   package a_package
 end
 
