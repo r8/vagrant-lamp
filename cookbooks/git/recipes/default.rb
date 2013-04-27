5e2a643c33bcacb1fb83b9b7e76fdc9cc5d6ec0e
Sergey Storchay <r8@r8.com.ua>
2013-04-27 21:44:13 +0300

    Update git cookbook

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -16,15 +16,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-case node[:platform]
-when "debian", "ubuntu"
-  package "git-core"
-when "centos","redhat","scientific","fedora"
-  case node[:platform_version].to_i
+case node['platform_family']
+when "debian"
+  if node['platform'] == "ubuntu" && node['platform_version'].to_f < 10.10
+    package "git-core"
+  else
+    package "git"
+  end
+when "rhel","fedora"
+  case node['platform_version'].to_i
   when 5
     include_recipe "yum::epel"
   end
   package "git"
+when "windows"
+  include_recipe 'git::windows'
+when "mac_os_x"
+  dmg_package "GitOSX-Installer" do
+    app node['git']['osx_dmg']['app_name']
+    package_id node['git']['osx_dmg']['package_id']
+    volumes_dir node['git']['osx_dmg']['volumes_dir']
+    source node['git']['osx_dmg']['url']
+    checksum node['git']['osx_dmg']['checksum']
+    type "pkg"
+    action :install
+  end
 else
   package "git"
 end
--- cookbooks/git/recipes/server.rb
+++ cookbooks/git/recipes/server.rb
@@ -16,19 +16,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "git"
-
-directory "/srv/git" do
-  owner "root"
-  group "root"
-  mode 0755
+if node["platform"] == "windows"
+  return "#{node['platform']} is not supported by the #{cookbook_name}::#{recipe_name} recipe"
 end
 
-case node[:platform]
-when "debian", "ubuntu"
+include_recipe "git"
+
+directory node["git"]["server"]["base_path"] do
+  owner "root"
+  group "root"
+  mode 00755
+end
+
+case node['platform_family']
+when "debian"
   include_recipe "runit"
-  runit_service "git-daemon"
+
+  package "git-daemon-run"
+
+  runit_service "git-daemon" do
+    sv_templates false
+  end
+when "rhel"
+  package "git-daemon"
+
+  template "/etc/xinetd.d/git" do
+    backup false
+    source "git-xinetd.d.erb"
+    owner "root"
+    group "root"
+    mode 00644
+  end
+
+  service "xinetd" do
+    action [:enable, :restart]
+  end
 else
   log "Platform requires setting up a git daemon service script."
-  log "Hint: /usr/bin/git daemon --export-all --user=nobody --group=daemon --base-path=/srv/git"
+  log "Hint: /usr/bin/git daemon --export-all --user=nobody --group=daemon --base-path=#{node["git"]["server"]["base_path"]}"
 end
