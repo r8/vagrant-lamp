9a9901f845bb9025ad1f0c3188c22e6c9567c143
Sergey Storchay <r8@r8.com.ua>
2013-09-13 11:24:07 +0300

    Update npm to 1.3.11

--- cookbooks/vagrant_main/attributes/default.rb
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -15,4 +15,4 @@ override['nodejs']['checksum'] = 'a102fad260d216b95611ddd57aeb6531c92ad103850839
 override['nodejs']['checksum_linux_x86'] = 'ea7332fcbbee8e33c2f7d9b0e75c9bb299f276b334b26752725aa8b9b0ee3c99'
 override['nodejs']['checksum_linux_x64'] = 'dcbad86b863faf4a1e10fec9ecd7864cebbbb6783805f1808f563797ce5db2b8'
 
-override['npm']['version'] = '1.3.4'
+override['npm']['version'] = '1.3.11'
