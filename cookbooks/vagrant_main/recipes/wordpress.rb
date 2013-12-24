42511e0db063e912c13e797753a8ba7ed8c67c5a
Sergey Storchay <r8@r8.com.ua>
2013-12-24 11:55:25 +0200

    Refactor wp-cli installer, close #71

--- cookbooks/vagrant_main/recipes/wordpress.rb
+++ cookbooks/vagrant_main/recipes/wordpress.rb
@@ -4,27 +4,8 @@ include_recipe "python"
 python_pip "wp2github"
 
 # Install wp-cli
-
-# Create wp-cli dir
-directory "/usr/share/wp-cli" do
-  recursive true
-end
-
-# Download installer
-remote_file "/usr/share/wp-cli/installer.sh" do
-  source 'http://wp-cli.org/installer.sh'
+remote_file "/usr/local/bin/wp" do                                              
+  source 'https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
   mode 0755
   action :create_if_missing
-end
-
-# Run installer
-bash 'install wp-cli' do
-  code './installer.sh'
-  cwd "/usr/share/wp-cli"
-  environment 'INSTALL_DIR' => "/usr/share/wp-cli"
-end
-
-# Link wp binary
-link "/usr/bin/wp" do
-  to "/usr/share/wp-cli/bin/wp"
 end
