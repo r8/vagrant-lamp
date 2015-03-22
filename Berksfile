d938342054c7cd5f35f3626c661cab641f77f5a1
Sergey Storchay <r8@r8.com.ua>
2015-03-22 10:35:20 +0200

    Fix node.js installation

--- Berksfile
+++ Berksfile
@@ -1,7 +1,7 @@
 source "https://supermarket.getchef.com"
 
-cookbook 'apt', '~> 2.6.1'
 cookbook 'apache2', '~> 3.0.1'
+cookbook 'apt', '~> 2.6.1'
 cookbook 'build-essential', '~> 2.1.3'
 cookbook 'composer', '~> 2.0.0'
 cookbook 'drush', '~> 0.9.0'
@@ -10,7 +10,7 @@ cookbook 'magento-toolbox', '~> 0.0.3'
 cookbook 'mysql', '~> 6.0.15'
 cookbook 'nodejs', '~> 2.2.0'
 cookbook 'npm', '~> 0.1.2'
+cookbook 'percona', '~> 0.15.5'
 cookbook 'phing', '~> 0.0.3'
 cookbook 'php', '~> 1.5.0'
 cookbook 'python', '~> 1.4.6'
-cookbook 'percona', '~> 0.15.5'
--- cookbooks/vagrant_main/attributes/default.rb
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -4,12 +4,6 @@ default['mysql']['port'] = '3306'
 
 override['percona']['apt_keyserver'] = 'keyserver.ubuntu.com'
 
-override['nodejs']['install_method'] = 'binary'
-override['nodejs']['version'] = '0.10.13'
-override['nodejs']['checksum'] = 'a102fad260d216b95611ddd57aeb6531c92ad1038508390654423feb1b51c059'
-override['nodejs']['checksum_linux_x86'] = 'ea7332fcbbee8e33c2f7d9b0e75c9bb299f276b334b26752725aa8b9b0ee3c99'
-override['nodejs']['checksum_linux_x64'] = 'dcbad86b863faf4a1e10fec9ecd7864cebbbb6783805f1808f563797ce5db2b8'
-
 override['npm']['version'] = '1.3.11'
 
 override['drush']['install_method'] = "git"
