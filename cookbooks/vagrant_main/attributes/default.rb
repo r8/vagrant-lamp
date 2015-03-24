bbfeb251021d2b8d747cc94c4858658cd4323bbd
Sergey Storchay <r8@r8.com.ua>
2015-03-24 14:14:59 +0200

    Fix merge issues

--- cookbooks/vagrant_main/attributes/default.rb
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -10,15 +10,8 @@ override['nodejs']['source']['checksum'] = '9700e23af4e9b3643af48cef5f2ad20a1331
 override['nodejs']['binary']['checksum']['linux_x64'] = '3bdb7267ca7ee24ac59c54ae146741f70a6ae3a8a8afd42d06204647fe9d4206'
 override['nodejs']['binary']['checksum']['linux_x86'] = 'd4130512228439bf9115b7057fe145b095c1e49fa8e62c8d3e192b3dd3fe821b'
 
-<<<<<<< HEAD
-override['npm']['version'] = '1.3.11'
-
-override['drush']['install_method'] = "git"
-override['drush']['version'] = "8.x-6.x"
-=======
 override['npm']['version'] = '2.7.4'
 
 override['postfix']['main']['relayhost'] = 'localhost:1025'
 
 include_attribute 'vagrant_main::php'
->>>>>>> develop
